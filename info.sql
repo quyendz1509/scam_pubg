2784af8c899a39203f58ded92440770e3438278e
quyendz1509 <quyendz1509@gmail.com>
2020-10-27 23:07:25 +0700

    database mới

--- info.sql
+++ info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 25, 2020 lúc 07:13 PM
+-- Thời gian đã tạo: Th10 27, 2020 lúc 05:06 PM
 -- Phiên bản máy phục vụ: 10.4.13-MariaDB
 -- Phiên bản PHP: 7.4.7
 
@@ -24,45 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `info`
+-- Cấu trúc bảng cho bảng `test`
 --
 
-CREATE TABLE `info` (
+CREATE TABLE `test` (
   `id` int(5) NOT NULL,
-  `taikhoan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `pass` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_id` int(5) NOT NULL,
-  `date` date NOT NULL
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `info`
+-- Đang đổ dữ liệu cho bảng `test`
 --
 
-INSERT INTO `info` (`id`, `taikhoan`, `pass`, `user_id`, `date`) VALUES
-(11, '123123', '123123', 123123, '2020-10-26'),
-(12, 'quyendz', 'quyendz', 123123, '2020-10-26'),
-(13, '123123123', '123123', 123123, '2020-10-26'),
-(14, '12312321312', '123123123123', 123123123, '2020-10-26'),
-(15, '1231q', '123123', 123, '2020-10-26'),
-(16, 'fsdfsd', 'sdfsdf', 423423, '2020-10-26'),
-(17, '123weqeq', '123123', 123123, '2020-10-26'),
-(18, '123123123qưe', '123123', 123123, '2020-10-26'),
-(19, 'acac', 'cacaca', 123123, '2020-10-26'),
-(20, '123123gfgf', '123123', 123123, '2020-10-26'),
-(21, '123123dfdf', '123123', 123123123, '2020-10-26'),
-(22, '1231312', '123123123', 123123123, '2020-10-26'),
-(23, 'fasdsa', 'asdasda', 1231231, '2020-10-26'),
-(24, '123123qweq', 'weqweq', 13123123, '2020-10-26');
+INSERT INTO `test` (`id`, `password`, `type`) VALUES
+(1, 'ab13a8c0ef17074e6ec0a49d0f855537', 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Chỉ mục cho bảng `info`
+-- Chỉ mục cho bảng `test`
 --
-ALTER TABLE `info`
+ALTER TABLE `test`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -70,10 +55,10 @@ ALTER TABLE `info`
 --
 
 --
--- AUTO_INCREMENT cho bảng `info`
+-- AUTO_INCREMENT cho bảng `test`
 --
-ALTER TABLE `info`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+ALTER TABLE `test`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
